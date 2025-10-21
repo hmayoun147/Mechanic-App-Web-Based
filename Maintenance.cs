917bd52760110ce1ea13afb28d9fde1019b24971
Hmayoun Nasir <hmayounnasir147@gmail.com>
2025-10-21 21:13:06 +0500

    Add files via upload

---
+++ Maintenance.cs
@@ -0,0 +1,40 @@
+﻿using MechanicAppWebBased.Models;
+using MechanicAppWebBased.ViewModels.Maintenance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MechanicAppWebBased.BusinessEntities.Maintenance
+{
+    public class Maintenance
+    {
+        EnterpriseSystemESEntities _db = new EnterpriseSystemESEntities();
+        TimeZoneInfo setTimeZoneInfo;
+        DateTime currentDateTime;
+        public tbl_UnScheduleMaintenanceModel RetriveMachineUnscheduleStatus(int MachineID)
+        {
+            return _db.Database.SqlQuery<tbl_UnScheduleMaintenanceModel>("Exec [dbo].[RetriveMachineUnscheduleStatus] {0}", new object[] { MachineID }).FirstOrDefault();
+        }
+        public tbl_UnScheduleMaintenanceModel SaveUnScheduleMaintenance(int MachineID, int CardNo, bool Started, bool Ended)
+        {
+            setTimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("Pakistan Standard Time");
+            currentDateTime = TimeZoneInfo.ConvertTime(DateTime.Now, setTimeZoneInfo);
+            return _db.Database.SqlQuery<tbl_UnScheduleMaintenanceModel>("Exec [dbo].[SaveUnScheduleMaintenance] {0},{1},{2},{3},{4},{5},{6},{7},{8}", new object[] { MachineID, CardNo, currentDateTime.ToString("yyyy-MM-dd HH:mm:ss"), currentDateTime.ToString("yyyy-MM-dd HH:mm:ss"), currentDateTime.ToString("hh:mm:ss"), currentDateTime.ToString("hh:mm:ss"), Started, Ended, currentDateTime.ToString("yyyy-MM-dd hh:mm:ss") }).FirstOrDefault();
+        }
+        public tbl_UnScheduleMaintenanceModel UpdateUnscheduleMaintenance(int MachineID, bool Ended)
+        {
+            setTimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("Pakistan Standard Time");
+            currentDateTime = TimeZoneInfo.ConvertTime(DateTime.Now, setTimeZoneInfo);
+            return _db.Database.SqlQuery<tbl_UnScheduleMaintenanceModel>("Exec [dbo].[UpdateUnscheduleMaintenance] {0},{1},{2},{3}", new object[] { MachineID, currentDateTime.ToString("yyyy-MM-dd HH:mm:ss"), currentDateTime.ToString("hh:mm:ss"), Ended }).FirstOrDefault();
+        }
+        public List<tbl_UnScheduleMaintenanceModel> RetriveDataTable()
+        { 
+            return _db.Database.SqlQuery<tbl_UnScheduleMaintenanceModel>("Exec [dbo].[RetriveDataTable]").ToList();
+        }
+        public List<tbl_MachineHistoryModel> RetriveMachineHistory()
+        {
+            return _db.Database.SqlQuery<tbl_MachineHistoryModel>("Exec [dbo].[RetriveMachineHistory]").ToList();
+        }
+    }
+}
---
+++ MaintenanceController.cs
@@ -0,0 +1,50 @@
+﻿using MechanicAppWebBased.BusinessEntities.Maintenance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MechanicAppWebBased.Controllers
+{
+    public class MaintenanceController : Controller
+    {
+        Maintenance ObjMain = new Maintenance();
+        // GET: Maintenance
+        public ActionResult NewRequest()
+        {
+            return PartialView();
+        }
+        public ActionResult RetriveMachineUnscheduleStatus()
+        {
+            return Json(ObjMain.RetriveMachineUnscheduleStatus(1), JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult SaveUnScheduleMaintenance()
+        {
+            bool Started = true;
+            bool Ended = false;
+            return Json(ObjMain.SaveUnScheduleMaintenance(2, 387, Started, Ended), JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult UpdateUnscheduleMaintenance()
+        {
+            bool Ended = true;
+            return Json(ObjMain.UpdateUnscheduleMaintenance(1, Ended), JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult ViewForm()
+        {
+            return PartialView();
+        }
+        public ActionResult RetriveDataTable()
+        {
+            return Json(ObjMain.RetriveDataTable(), JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult MachineHistory()
+        {
+            return PartialView();
+        }
+        public ActionResult RetriveMachineHistory()
+        {
+            return Json(ObjMain.RetriveMachineHistory(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
